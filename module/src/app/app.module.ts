40c09e4cb413712c04ebb6c9fdc2d37e9dddb880
Ansi Kunjumon PK <ansikunjumon@gmail.com>
2021-02-05 15:30:14 +0530

    module

---
+++ module/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import{UsersModule } from './users/users.module'
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,UsersModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
